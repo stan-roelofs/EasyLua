0bcf1d46b530a6fe9f1d213c9110c4105f06d3c2
Stan Roelofs <stan@stanroelofs.nl>
2022-10-14 22:59:48 +0200

    Create / destroy lua state

--- include/lua_wrapper/lua_wrapper.h
+++ include/lua_wrapper/lua_wrapper.h
@@ -1,12 +1,27 @@
 #ifndef __LUA_WRAPPER_HPP
 #define __LUA_WRAPPER_HPP
 
+#include <lua.hpp>
+#include <stdexcept>
+#include <memory>
+#include <string>
+
 namespace lua_wrapper
 {
+    class OutOfMemoryException : public std::runtime_error
+    {
+    public:
+        OutOfMemoryException(std::string msg) : std::runtime_error(msg) {}
+    };
+
     class LuaContext final
     {
+    public:
         LuaContext();
         ~LuaContext();
+
+    private:
+        lua_State *lua_state_;
     };
 }
 
