8722d56251c9732ada98ad426829dc541443a839
Stan Roelofs <stan@stanroelofs.nl>
2022-11-26 22:53:16 +0100

    Added tests for script class

---
+++ test/src/script.cpp
@@ -0,0 +1,34 @@
+#include <easylua/script.hpp>
+
+#include <gtest/gtest.h>
+
+class Script : public ::testing::Test
+{
+public:
+    Script()
+    {
+        L = luaL_newstate();
+        if (!L)
+            throw std::runtime_error("Could not create Lua state");
+    }
+
+    ~Script()
+    {
+        lua_close(L);
+    }
+
+protected:
+    lua_State *L;
+};
+
+using namespace easylua;
+
+TEST_F(Script, load_string_succeeds)
+{
+    ASSERT_EQ(script::LoadResult::kOK, script::FromString(L, "function test() end"));
+}
+
+TEST_F(Script, load_string_fails_on_syntax_error)
+{
+    ASSERT_EQ(script::LoadResult::kSyntaxError, script::FromString(L, "function test()"));
+}
