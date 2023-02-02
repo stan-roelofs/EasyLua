a49dacff54c1a371818937bdac46687cdd2d95ed
Stan Roelofs <stan@stanroelofs.nl>
2023-02-02 22:17:43 +0100

    Added safe_reference and unsafe_reference

--- include/easylua/reference.hpp
+++ include/easylua/reference.hpp
@@ -3,20 +3,71 @@
 
 #include <lua.hpp>
 
+#include "exception.hpp"
+
 namespace easylua
 {
     class reference
     {
     public:
-        reference(lua_State *state, int index) : lua_state_(state), reference_(LUA_NOREF)
+        virtual ~reference() = default;
+
+        /// @brief Pushes the referenced value onto the stack.
+        virtual void push() const = 0;
+    };
+
+    class unsafe_reference : public reference
+    {
+    public:
+        void push() const override
+        {
+            lua_pushvalue(lua_state_, index_);
+        }
+
+    protected:
+        unsafe_reference(lua_State *state, int index) : lua_state_(state), index_(index)
         {
             if (!lua_state_)
                 throw invalid_argument("state", "cannot be null");
 
+            if (index == 0)
+                throw invalid_argument("index", "cannot be 0");
+
+            if (lua_type(lua_state_, index_) == LUA_TNONE)
+                throw invalid_argument("index", "cannot be LUA_TNONE");
+        }
+
+        lua_State *lua_state_;
+        int index_;
+    };
+
+    class safe_reference : public reference
+    {
+    public:
+        void push() const override
+        {
+            if (reference_ == LUA_NOREF)
+                throw runtime_error("cannot push a safe_reference that is not valid");
+
+            lua_rawgeti(lua_state_, LUA_REGISTRYINDEX, reference_);
+        }
+
+    protected:
+        safe_reference(lua_State *state, int index) : lua_state_(state), reference_(LUA_NOREF)
+        {
+            if (!lua_state_)
+                throw invalid_argument("state", "cannot be null");
+
+            if (index == 0)
+                throw invalid_argument("index", "cannot be 0");
+
+            if (lua_type(lua_state_, index) == LUA_TNONE)
+                throw invalid_argument("index", "cannot be LUA_TNONE");
+
             reference_ = luaL_ref(lua_state_, LUA_REGISTRYINDEX);
         }
 
-        reference(const reference &other) : lua_state_(other.lua_state_), reference_(LUA_NOREF)
+        safe_reference(const safe_reference &other) : lua_state_(other.lua_state_), reference_(LUA_NOREF)
         {
             if (other.reference_ != LUA_NOREF)
             {
@@ -25,12 +76,12 @@ namespace easylua
             }
         }
 
-        reference(reference &&other) : lua_state_(other.lua_state_), reference_(other.reference_)
+        safe_reference(safe_reference &&other) : lua_state_(other.lua_state_), reference_(other.reference_)
         {
             other.reference_ = LUA_NOREF;
         }
 
-        reference &operator=(const reference &other)
+        safe_reference &operator=(const safe_reference &other)
         {
             if (this != &other)
             {
@@ -50,7 +101,7 @@ namespace easylua
             return *this;
         }
 
-        reference &operator=(reference &&other)
+        safe_reference &operator=(safe_reference &&other)
         {
             if (this != &other)
             {
@@ -66,21 +117,12 @@ namespace easylua
             return *this;
         }
 
-        ~reference()
+        ~safe_reference()
         {
             if (reference_ != LUA_NOREF)
                 luaL_unref(lua_state_, LUA_REGISTRYINDEX, reference_);
         }
 
-        void push() const
-        {
-            if (reference_ == LUA_NOREF)
-                throw runtime_error("cannot push a reference that is not valid");
-
-            lua_rawgeti(lua_state_, LUA_REGISTRYINDEX, reference_);
-        }
-
-    private:
         lua_State *lua_state_;
         int reference_;
     };
---
+++ test/src/reference.cpp
@@ -0,0 +1,89 @@
+#include <easylua/reference.hpp>
+
+#include <gtest/gtest.h>
+
+struct mock_unsafe_reference : easylua::unsafe_reference
+{
+    mock_unsafe_reference(lua_State *state, int index) : unsafe_reference(state, index) {}
+};
+
+TEST(unsafe_reference, constructor_throws_on_null_state)
+{
+    EXPECT_THROW(mock_unsafe_reference(nullptr, 0), easylua::invalid_argument);
+}
+
+TEST(unsafe_reference, constructor_throws_on_zero_index)
+{
+    lua_State *L = luaL_newstate();
+    EXPECT_THROW(mock_unsafe_reference(L, 0), easylua::invalid_argument);
+    lua_close(L);
+}
+
+TEST(unsafe_reference, constructor_throws_on_invalid_index)
+{
+    lua_State *L = luaL_newstate();
+    EXPECT_THROW(mock_unsafe_reference(L, 1), easylua::invalid_argument);
+    lua_close(L);
+}
+
+TEST(unsafe_reference, push)
+{
+    lua_State *L = luaL_newstate();
+    lua_pushnumber(L, 5);
+    mock_unsafe_reference ref(L, 1);
+    ref.push();
+    ASSERT_EQ(LUA_TNUMBER, lua_type(L, -1));
+    ASSERT_EQ(5, lua_tonumber(L, -1));
+    lua_close(L);
+}
+
+struct mock_safe_reference : easylua::safe_reference
+{
+    mock_safe_reference(lua_State *state, int index) : safe_reference(state, index) {}
+
+    int &reference()
+    {
+        return reference_;
+    }
+};
+
+TEST(safe_reference, constructor_throws_on_null_state)
+{
+    EXPECT_THROW(mock_safe_reference(nullptr, 0), easylua::invalid_argument);
+}
+
+TEST(safe_reference, constructor_throws_on_zero_index)
+{
+    lua_State *L = luaL_newstate();
+    EXPECT_THROW(mock_safe_reference(L, 0), easylua::invalid_argument);
+    lua_close(L);
+}
+
+TEST(safe_reference, constructor_throws_on_invalid_index)
+{
+    lua_State *L = luaL_newstate();
+    EXPECT_THROW(mock_safe_reference(L, 1), easylua::invalid_argument);
+    lua_close(L);
+}
+
+// TODO copy, move
+
+TEST(safe_reference, push)
+{
+    lua_State *L = luaL_newstate();
+    lua_pushnumber(L, 5);
+    mock_safe_reference ref(L, 1);
+    // ref.push();
+    ASSERT_EQ(LUA_TNUMBER, lua_type(L, -1));
+    ASSERT_EQ(5, lua_tonumber(L, -1));
+    lua_close(L);
+}
+
+TEST(safe_reference, push_throws_with_invalid_reference)
+{
+    lua_State *L = luaL_newstate();
+    lua_pushnumber(L, 5);
+    mock_safe_reference ref(L, 1);
+    ref.reference() = LUA_NOREF;
+    EXPECT_THROW(ref.push(), easylua::runtime_error);
+}
